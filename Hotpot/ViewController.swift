7a01a0cb96aae0950b18b3e13a0619d826a8569d
Jingyan Dong <jingyandong@Jingyans-MacBook-Pro.local>
2018-04-02 19:53:24 -0400

    branching from master

--- Hotpot/ViewController.swift
+++ Hotpot/ViewController.swift
@@ -2,21 +2,50 @@
 //  ViewController.swift
 //  Hotpot
 //
-//  Created by Jingyan Dong on 2/26/18.
+//  Created by Jingyan Dong on 4/2/18.
 //  Copyright © 2018 Jingyan Dong. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var btnSelect: UIButton!
-    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    //@IBOutlet weak var menuButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        sideMenus()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
+    func sideMenus() {
+        if revealViewController() != nil {
+            
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
